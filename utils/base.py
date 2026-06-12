3ae51805e81e2aaaed5586e5bf3d91a98dedafb6
Rodrigo <rodrigo@example.com>
2026-06-11 21:48:17 -0300

    style: implement UX improvements in dashboard and search pages

--- utils/base.py
+++ utils/base.py
@@ -321,24 +321,38 @@ def salvar_csv(
             # Adiciona nova entrada
             def get_source_from_filename(filename: str) -> str:
                 fn = filename.lower()
-                if fn.startswith('standard_and_poors_') or fn.startswith('s_p_'):
-                    return 'S&P'
-                elif fn.startswith('moodys_'):
-                    return "Moody's"
-                elif fn.startswith('fitch_'):
-                    return 'Fitch'
+                if 'standard_and_poors' in fn or 's_p' in fn:
+                    return 's_p'
+                elif 'moodys' in fn:
+                    return 'moodys'
+                elif 'fitch' in fn:
+                    return 'fitch'
+                elif 'austin' in fn:
+                    return 'austin'
+                elif 'liberum' in fn:
+                    return 'liberum'
+                elif 'consolidado' in fn:
+                    return 'consolidated'
                 return 'other'
 
             def get_title_from_filename(filename: str) -> str:
                 fn = filename.lower()
+                if 'consolidado' in fn:
+                    return 'Emissores Consolidados'
+                
                 if 'standard_and_poors' in fn or 's_p' in fn:
                     ag = 'S&P'
                 elif 'moodys' in fn:
                     ag = "Moody's"
                 elif 'fitch' in fn:
                     ag = 'Fitch'
+                elif 'austin' in fn:
+                    ag = 'Austin'
+                elif 'liberum' in fn:
+                    ag = 'Liberum'
                 else:
                     ag = filename.replace('.csv', '').replace('_', ' ').title()
+                
                 if 'entidades' in fn or 'emissores' in fn:
                     suf = 'Emissores'
                 else:
