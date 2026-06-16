e19b49b9bc923ce51bff286d79c4509aa426e550
Rodrigo <rodrigo@example.com>
2026-06-16 08:29:04 -0300

    chore: explicita bind 0.0.0.0 no http.server

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /app
 
-python -m http.server 80 &
+python -m http.server 80 --bind 0.0.0.0 &
 
 echo "0 0 * * * cd /app && python run_all.py >> /var/log/cron.log 2>&1" \
     > /etc/cron.d/pulse-ratings-cron \
